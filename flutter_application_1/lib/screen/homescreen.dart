63f680a679bdb1a97ed541b257649246406efc42
PholNaja <33473976+PHOLnaja@users.noreply.github.com>
2024-05-17 11:48:43 +0700

    example

--- flutter_application_1/lib/screen/homescreen.dart
+++ flutter_application_1/lib/screen/homescreen.dart
@@ -8,29 +8,91 @@ class Homescreen extends StatefulWidget {
 class _HomescreenState extends State<Homescreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.center,
+    double h = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Column(
         children: [
-          Column(
+          Image.asset('assets/img/picture1.jpg'),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                  Text(
+                    'Manchester United',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w700,
+                    ),
+                    textAlign: TextAlign.start,
+                  ),
+                  Text('Manchester United Football Club',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w500,
+                      )),
+                ]),
+                Row(children: [
+                  Icon(Icons.star, color: Colors.red.shade300),
+                  Text("41"),
+                ]),
+              ],
+            ),
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Icon(Icons.call),
-              Text("Call"),
+              Column(
+                children: [
+                  Icon(Icons.call),
+                  Text("Call"),
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(Icons.route),
+                  Text("Route"),
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(Icons.share),
+                  Text("Share"),
+                ],
+              )
             ],
           ),
-          Column(
-            children: [
-              Icon(Icons.route),
-              Text("ROUTE"),
-            ],
-          ),
-          Column(
-            children: [
-              Icon(Icons.share),
-              Text("Share"),
-            ],
-          ),
+          Container(
+            height: h * 0.6,
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Text(
+                      "Manchester United Football Club, commonly referred to as Man United (often stylised as Man Utd), or simply United, is a professional football club based in Old Trafford, Greater Manchester, England. The club competes in the Premier League, the top division in the English football league system. Nicknamed the Red Devils, they were founded as Newton Heath LYR Football Club in 1878, but changed their name to Manchester United in 1902. After a spell playing in Clayton, Manchester, the club moved to their current stadium, Old Trafford, in 1910.",
+                      style: TextStyle(
+                        fontSize: 14.0,
+                      ),
+                      textAlign: TextAlign.justify,
+                    ),
+                    Text(
+                      "Domestically, Manchester United have won a record 20 top-flight league titles, 12 FA Cups, 6 League Cups and a record 21 FA Community Shields. In international football, they have won the European Cup/UEFA Champions League three times, and the UEFA Europa League, the UEFA Cup Winners' Cup, the UEFA Super Cup, the Intercontinental Cup and the FIFA Club World Cup once each.[6][7] In 1968, under the management of Matt Busby, 10 years after eight of the club's players were killed in the Munich air disaster, they became the first English club to win the European Cup. Sir Alex Ferguson is the club's longest-serving and most successful manager, winning 38 trophies, including 13 league titles, five FA Cups, and two Champions League titles between 1986 and 2013.[8][9] In the 1998–99 season, under Ferguson, the club became the first in the history of English football to achieve the continental treble of the Premier League, FA Cup and UEFA Champions League.[10] In winning the UEFA Europa League under José Mourinho in 2016–17, they became one of five clubs to have won the original three main UEFA club competitions (the Champions League, Europa League and Cup Winners' Cup).",
+                      style: TextStyle(
+                        fontSize: 14.0,
+                      ),
+                      textAlign: TextAlign.justify,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
